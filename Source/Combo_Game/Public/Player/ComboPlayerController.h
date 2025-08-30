6075a5975e50300f7cf57043d6618d953a57f05b
bc-huang77 <bc_huang@foxmail.com>
2025-08-30 22:27:18 +0800

    Init Player base class

---
+++ Source/Combo_Game/Public/Player/ComboPlayerController.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "ComboPlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COMBO_GAME_API AComboPlayerController : public APlayerController
+{
+	GENERATED_BODY()
+	
+};
